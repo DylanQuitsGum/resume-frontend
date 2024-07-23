2e392a61f7ed1188ef2db9b3c744e3de80534ed8
DylanQuitsGum <greg.satterlee@Gmail.com>
2024-07-23 23:23:28 +0100

    add duty is working

--- src/components/AddDuty.vue
+++ src/components/AddDuty.vue
@@ -1,7 +1,85 @@
 <template>
-    <p>Add Duty</p>
-</template>
+    <div class="submit-form mt-3 mx-auto">
+      <h2 class="headline">Add Duty</h2>
+  
+      <div v-if="!submitted">
+        <v-form ref="form" lazy-validation>
+          <v-text-field
+            variant="outlined"
+            v-model="duty.dutyText"
+            :rules="[(v) => !!v || 'Duty is required']"
+            label="Job Duty"
+            required
+          ></v-text-field>
+        </v-form>
+  
+        <v-btn color="primary" class="mt-3" @click="saveDuty">Submit</v-btn>
+      </div>
+  
+      <div v-else>
+        <v-card class="mx-auto">
+          <v-card-title> Submitted successfully! </v-card-title>
+  
+          <v-card-subtitle> Click the button to add new duty. </v-card-subtitle>
+  
+          <v-card-actions>
+            <v-btn color="success" @click="newDuty">Add</v-btn>
+            <v-btn color="success" @click="listDuties">Return</v-btn>
+          </v-card-actions>
+        </v-card>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from "vue";
+  import { useRoute, useRouter } from 'vue-router';
+  import DutyService from "@/services/duty.service";
 
-<script setup>
+  const route = useRoute();
+  const router = useRouter();
+  
+  const user = ref(JSON.parse(localStorage.getItem("user")));
+  const submitted = ref(false);
+  const employerId = ref();
 
-</script>
+  const duty = ref({
+    dutyId: null,
+    dutyText: "GitHub",
+  });
+  
+  const saveDuty = async () => {
+    var data = {
+      dutyText: duty.value.dutyText,
+      employerId: employerId.value,
+    };
+    DutyService.create(employerId.value, data)
+      .then((response) => {
+        duty.value.id = response.data.id;
+        submitted.value = true;
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+  
+  const newDuty = async () => {
+    submitted.value = false;
+    duty.value = {};
+  };
+  
+  const listDuties = async () => {
+    router.push({ path: `/jobDuties/${employerId.value}` });
+  };
+  
+  onMounted(() => {
+    employerId.value = route.params.id;
+  });
+  </script>
+  
+  <style>
+  .submit-form {
+    max-width: 300px;
+  }
+  </style>
+  
